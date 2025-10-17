e64222c4d15f68593a04d6d305cdfb8106151d7b
max-shelll <max-shelll@proton.me>
2025-10-17 22:57:44 +0300

    Simplify UpdateControllerFactory

--- dispatcher/src/main/java/com/maxshelll/dispatcher/controller/UpdateControllerFactory.java
+++ dispatcher/src/main/java/com/maxshelll/dispatcher/controller/UpdateControllerFactory.java
@@ -2,31 +2,32 @@ package com.maxshelll.dispatcher.controller;
 
 import com.maxshelll.dispatcher.controller.update.UpdateController;
 import com.maxshelll.dispatcher.dto.UpdateType;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
-@Controller
-@RequiredArgsConstructor
+@Component
 public class UpdateControllerFactory {
 
-    private final List<UpdateController> controllers;
-    private final Map<UpdateType, UpdateController> controllerMap = new EnumMap<>(UpdateType.class);
+    private final Map<UpdateType, UpdateController> controllers;
 
-    @PostConstruct
-    public void init() {
-        for (UpdateController controller : controllers) {
+    public UpdateControllerFactory(List<UpdateController> controllerList) {
+        this.controllers = createControllerMap(controllerList);
+    }
+
+    private Map<UpdateType, UpdateController> createControllerMap(List<UpdateController> controllerList) {
+        Map<UpdateType, UpdateController> controllerMap = new EnumMap<>(UpdateType.class);
+        for (UpdateController controller : controllerList) {
             controllerMap.put(controller.getType(), controller);
         }
+
+        return controllerMap;
     }
 
     public UpdateController getController(Update update) {
-        UpdateType type = UpdateType.fromUpdate(update);
-        return controllerMap.get(type);
+        return controllers.get(UpdateType.fromUpdate(update));
     }
 }
