b1d0711d9823b293e5d77f342f532509c1e4ff48
max-shelll <max-shelll@proton.me>
2025-10-17 23:32:07 +0300

    Remove unused classes

--- dispatcher/src/main/java/com/maxshelll/dispatcher/controller/update/MessageUpdateController.java
+++ dispatcher/src/main/java/com/maxshelll/dispatcher/controller/update/MessageUpdateController.java
@@ -1,6 +1,5 @@
 package com.maxshelll.dispatcher.controller.update;
 
-import com.maxshelll.dispatcher.dto.UpdateType;
 import com.maxshelll.dispatcher.service.rabbitMQ.producer.ProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,19 +7,13 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Controller
 @RequiredArgsConstructor
-public class MessageUpdateController implements UpdateController {
+public class MessageUpdateController {
 
     private final ProducerService producerService;
 
-    @Override
     public void request(Update update) {
         if (update.hasMessage() && update.getMessage().hasText())
             producerService.produceMessage(update);
     }
 
-
-    @Override
-    public UpdateType getType() {
-        return UpdateType.MESSAGE;
-    }
 }
--- dispatcher/src/main/java/com/maxshelll/dispatcher/controller/update/UpdateController.java
+++
@@ -1,9 +0,0 @@
-package com.maxshelll.dispatcher.controller.update;
-
-import com.maxshelll.dispatcher.dto.UpdateType;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-public interface UpdateController {
-    void request(Update update);
-    UpdateType getType();
-}
